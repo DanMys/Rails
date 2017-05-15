97d5576039a3ba82a0c7d6164fd45e4ddb375b38
DanMys <iniguez.daniel@uabc.edu.mx>
2017-05-14 22:16:02 -0700

    FileCabinet
    Added the new Gems, generated the welcome controller w/index action,
    generated the model Doc and the Docs controller

---
+++ cabinet/app/controllers/docs_controller.rb
@@ -0,0 +1,41 @@
+class DocsController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  # No tiene una vista tal cual
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  # No tiene una vista tal cual
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def find_doc
+
+    end
+
+    def doc_params
+
+    end
+end
