1312ebc86580a77298821c7fe993a1c4eb816819
DanMys <iniguez.daniel@uabc.edu.mx>
2017-06-30 00:37:38 -0700

    Instagram Clone
    Edit and Delete Posts, Add User model and his association with pics
    model, styling and navigation

--- instagramm/app/controllers/pics_controller.rb
+++ instagramm/app/controllers/pics_controller.rb
@@ -5,21 +5,36 @@ class PicsController < ApplicationController
   end
 
   def show
-
   end
 
   def new
-    @pic = Pic.new
+    @pic = current_user.pics.build
   end
 
   def create
-    @pic = Pic.new(pic_params)
+    @pic = current_user.pics.build(pic_params)
 
     if @pic.save
       redirect_to @pic, notice: "Yesss! It was posted!"
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: "Congrats! Pic was updated!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
   end
 
 
